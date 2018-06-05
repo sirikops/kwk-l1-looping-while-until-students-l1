6829d596e9393a33d20d489e023e2d8eaad13707
sirikops <sirikops@hotmail.com>
2018-06-05 21:23:50 +0000

    Done.

---
+++ until.rb
@@ -0,0 +1,11 @@
+def levitation_charm2
+  count=6
+  until count==10 
+  puts "Wingardium Leviosa"
+  count+=1
+  puts "#{count} charm(s) completed"
+end 
+end
+levitation_charm2
+
+
---
+++ while.rb
@@ -0,0 +1,10 @@
+def levitation_charm
+  count=6
+  
+  while count<10 
+  puts "Wingardium Leviosa"
+  count+=1
+  end
+end
+levitation_charm
+ 
